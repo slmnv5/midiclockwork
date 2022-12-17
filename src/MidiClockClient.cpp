ffcb70d44385a0bc7e014c415c1e85f1bd565d74
slmnv5 <slmnv5@gmail.com>
2022-12-16 22:20:39 -0500

    1

--- src/MidiClockClient.cpp
+++ src/MidiClockClient.cpp
@@ -18,10 +18,10 @@ void MidiClockClient::run()
         send_event(&event_start);
         LOG(LogLvl::DEBUG) << "MIDI clock running: " << !stopped;
         auto begin = std::chrono::steady_clock::now();
-        double sum, sum2;
+        double sum, sum2, max;
         while (!stopped)
         {
-            sum = sum2 = 0;
+            sum = sum2 = max = 0;
             for (int k = 0; k < 96; k++)
             {
                 usleep(sleep_time_us);
@@ -30,16 +30,17 @@ void MidiClockClient::run()
                 std::chrono::duration<double> diff = end - begin;
                 begin = end;
                 auto secs = diff.count();
+                max = std::max(max, abs(secs));
                 sum += (secs);
                 sum2 += (secs * secs);
             }
             auto aver = sum / 96;
             auto aver2 = sum2 / 96;
             auto stdev = sqrt(aver2 - aver * aver);
-            LOG(LogLvl::DEBUG) << "\tabs. error (ms): "
-                               << std::fixed << std::setprecision(3)
-                               << (aver - sleep_time) * 1000
+            LOG(LogLvl::DEBUG) << std::fixed << std::setprecision(3)
+                               << "\taver. error (ms): " << (aver - sleep_time) * 1000
                                << "\tstd. dev (ms): " << stdev * 1000
+                               << "\tmax (ms): " << max * 1000
                                << "\trel. error (%): " << (stdev / sleep_time * 100);
         }
     }
